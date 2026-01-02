25c0c0f331ea901bfde1ad68b362c13a405ddfb6
pucciquinn@gmail.com <m@ms-MacBook-Air.local>
2026-01-02 10:16:39 -0700

    created get response and started should simulate

--- src/TakeOutSimulator.java
+++ src/TakeOutSimulator.java
@@ -5,10 +5,36 @@ public class TakeOutSimulator{
     private FoodMenu menu;
     private Scanner input;
 
-    public TakeOutSimulator(customer, menu, input){
+    public TakeOutSimulator(Customer customer, FoodMenu menu, Scanner input){
         this.customer = customer;
         this.menu = menu;
         this.input = input;
+    }
+
+    private <T> T getResponse(String userInputPrompt, UserInputRetriever<T> userInputRetriever){
+        while (true) {
+            try {
+                System.out.println(userInputPrompt);
+                String usrInput = input.nextLine();
+
+                int value = Integer.parseInt(userInput);
+
+                return userInputRetriever.produceOutput(value);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Input needs to be an `int` type.")
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(e.getMessage())
+            }
+        }
+    }
+
+    boolean shouldSimulate() {
+        String userPrompt = " type '1' to proceed with simulation, '0' to exit"
+        try {
+
+        }
     }
 
 }
